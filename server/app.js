836f378b72824f9a74d75abd86c7ceac103a7c11
Arshia Shahidi <arshia.shahidi@gmail.com>
2021-10-14 03:47:24 +0300

    RootQuery - Running my first query in GraphiQL

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const GraphQL = require('express-graphql')
 
+const schema = require('./schema/schema')
+
 const app = express()
 
 
 
 app.use('/graphql', GraphQL.graphqlHTTP({
-    graphiql: true
+    graphiql: true,
+    schema
 }))
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,8 +5,32 @@ const UserType = new graphql.GraphQLObjectType({
     name: "User",
     description: "Documentation for user...",
     fields: () => ({
-        id: { type: graphql.GraphQLID },
+        id: { type: graphql.GraphQLString },
         name: { type: graphql.GraphQLString },
         age: { type: graphql.GraphQLInt }
     })
+})
+
+
+// RootQuery
+const RootQuery = new graphql.GraphQLObjectType({
+    name: "RootQueryType",
+    description: "Description...",
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: graphql.GraphQLString } },
+
+            resolve(parent, args) {
+                // We resolve with data
+                // get and return data from a datasource
+
+            }
+        }
+    }
+})
+
+
+module.exports = new graphql.GraphQLSchema({
+    query: RootQuery
 })
