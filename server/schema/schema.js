7f7aedde5035f0bb15e86fd9fe21be0813c708c5
Arshia Shahidi <arshia.shahidi@gmail.com>
2021-10-14 04:10:30 +0300

    +Returning Data in the Resolver Function.\n+Adding Lodash library.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,15 @@
 const graphql = require('graphql')
+const _ = require('lodash') 
+
+// dummy data
+let usersData = [
+    { id: "1", name: "Arshia", age: 21, status: "Software Developer" },
+    { id: "2", name: "Mahmut", age: 15, status: "Student" },
+    { id: "3", name: "Nesli", age: 26, status: "Student" },
+    { id: "4", name: "Ibrahim", age: 44 },
+    { id: "5", name: "Fatma", age: 24, status: "Student" },
+    { id: "6", name: "Ayse", age: 32, status: "Engineer" },
+]
 
 // Create types
 const UserType = new graphql.GraphQLObjectType({
@@ -7,7 +18,8 @@ const UserType = new graphql.GraphQLObjectType({
     fields: () => ({
         id: { type: graphql.GraphQLString },
         name: { type: graphql.GraphQLString },
-        age: { type: graphql.GraphQLInt }
+        age: { type: graphql.GraphQLInt },
+        status: { type: graphql.GraphQLString }
     })
 })
 
@@ -22,6 +34,8 @@ const RootQuery = new graphql.GraphQLObjectType({
             args: { id: { type: graphql.GraphQLString } },
 
             resolve(parent, args) {
+                return _.find(usersData, { id: args.id })
+
                 // We resolve with data
                 // get and return data from a datasource
 
